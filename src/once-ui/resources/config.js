27191739156c6003c9dd342cbe20b70333109a48
Abhinav Singh <161646500+aviiix96@users.noreply.github.com>
2025-02-12 08:02:32 -0500

    Update config.js

--- src/once-ui/resources/config.js
+++ src/once-ui/resources/config.js
@@ -16,33 +16,18 @@ const style = {
 
 // default metadata
 const meta = {
-  title: "Once UI - Your Personalized Design System for Next.js",
+  title: "Abhinav's Portfolio",
   description:
-    "An open-source design system and component library for Next.js that emphasizes easy styling and accessibility in UI development.",
+    "Portfolio",
 };
 
 // default open graph data
 const og = {
-  title: "Once UI for Next.js",
-  description: "We let designers code and developers design.",
+  title: "Abhinav",
+  description: "portfolio.",
   type: "website",
-  image: "/images/cover.jpg"
 };
 
-// default schema data
-const schema = {
-  logo: "",
-  type: "Organization",
-  name: "One UI",
-  description: "Once UI is an open-source design system and component library for Next.js.",
-  email: "",
-};
 
-// social links
-const social = {
-  twitter: "https://www.twitter.com/_onceui",
-  linkedin: "https://www.linkedin.com/company/once-ui/",
-  discord: "https://discord.com/invite/5EyAQ4eNdS",
-};
 
 export { baseURL, style, meta, og, schema, social };
